ddc0e6471add4c6a72c33fdebbc7b71748c7d213
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-07-18 22:09:58 -0300

    feat: format

--- src/main/java/com/portfoliodeveloper/controller/developer/DeveloperUpdateController.java
+++ src/main/java/com/portfoliodeveloper/controller/developer/DeveloperUpdateController.java
@@ -7,10 +7,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
